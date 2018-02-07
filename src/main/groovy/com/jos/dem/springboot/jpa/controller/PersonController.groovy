60cdfe6c9b73aa066d16ac47981907f9349713c7
josdem <joseluis.delacruz@gmail.com>
2018-02-07 10:51:51 -0600

    Adding save person method

--- src/main/groovy/com/jos/dem/springboot/jpa/controller/PersonController.groovy
+++ src/main/groovy/com/jos/dem/springboot/jpa/controller/PersonController.groovy
@@ -1,12 +1,16 @@
 package com.jos.dem.springboot.jpa.controller
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET
+import static org.springframework.web.bind.annotation.RequestMethod.POST
+
+import javax.validation.Valid
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.servlet.ModelAndView
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.validation.BindingResult
 
 import com.jos.dem.springboot.jpa.model.Person
 import com.jos.dem.springboot.jpa.command.Command
@@ -40,6 +44,22 @@ class PersonController {
 		ModelAndView modelAndView = new ModelAndView('persons/create')
 		Command personCommand = new PersonCommand()
 		modelAndView.addObject('personCommand', personCommand)
+		modelAndView
+	}
+
+	@RequestMapping(method=POST)
+	ModelAndView save(@Valid PersonCommand personCommand, BindingResult bindingResult){
+		log.info "Registering new Person: ${personCommand.nickname}"
+		ModelAndView modelAndView = new ModelAndView('list')
+		if(bindingResult.hasErrors()){
+			modelAndView.setViewName('persons/create')
+			modelAndView.addObject('personCommand', personCommand)
+			return modelAndView
+		}
+		Person person = new Person(nickname:personCommand.nickname, email:personCommand.email)
+		personRepository.save(person)
+		List<Person> persons = personRepository.findAll()
+		modelAndView.addObject('persons', persons)
 		modelAndView
 	}
 	
